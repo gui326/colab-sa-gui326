3aa0a94831452d01c5e45efae87103d0a5f28df7
Gui326 <guilherme.326@hotmail.com>
2026-02-25 14:45:08 -0300

    chore: Docker e DB

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,6 +21,19 @@ import databaseConfig from './config/database.config';
           limit: 10,
         },
       ],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
   ],
   controllers: [AppController],
